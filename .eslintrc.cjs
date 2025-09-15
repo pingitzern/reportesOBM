2f92afd266fbce48ee372211769df0c8caf223ea
pingitzern <133623572+pingitzern@users.noreply.github.com>
2025-09-15 20:46:17 -0300

    Setup linting and formatting and fix warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,12 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2022: true,
-    },
-    extends: ['eslint:recommended'],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    globals: {
-        process: 'readonly',
-        Chart: 'readonly',
-    },
-    ignorePatterns: ['dist/', 'node_modules/'],
-    rules: {
-        'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^ignored' }],
-        'no-console': 'off',
-    },
-    overrides: [
-        {
-            files: ['*.config.js', 'postcss.config.js', 'tailwind.config.js', 'vite.config.js'],
-            env: {
-                node: true,
-            },
-        },
-        {
-            files: ['scripts/**/*.js'],
-            env: {
-                node: true,
-            },
-        },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
 };
